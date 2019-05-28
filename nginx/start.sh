a2316ccd7396de74c35b610205395595467fa639
Alexander Kiel <alexanderkiel@gmx.net>
2019-05-28 16:03:40 +0200

    Initial Compose File

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-sed -i "s#<searchbroker-ui>#${SEARCHBROKER_UI:-searchbroker-ui}#" /etc/nginx/nginx.conf
-sed -i "s#<searchbroker>#${SEARCHBROKER:-searchbroker}#" /etc/nginx/nginx.conf
+sed -i "s#<searchbroker-ui>#${SEARCHBROKER_UI:-searchbroker-ui:8080}#" /etc/nginx/nginx.conf
+sed -i "s#<searchbroker>#${SEARCHBROKER:-searchbroker:8080}#" /etc/nginx/nginx.conf
 
 # Replace start.sh with nginx
 exec nginx -g "daemon off;"
